a63f4c55d12fa506eb0905c6ef53726964e04e51
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-05-12 13:48:09 -0700

    Updated sql queries to insert records

--- sql_queries.py
+++ sql_queries.py
@@ -10,7 +10,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 songplay_table_create = ("""
 CREATE TABLE songplays (
-    songplay_id bigint PRIMARY KEY,
+    songplay_id SERIAL PRIMARY KEY,
     start_time timestamp,
     user_id varchar(10),
     level varchar(10),
@@ -20,9 +20,7 @@ CREATE TABLE songplays (
     location varchar(200),
     user_agent varchar(200),
     FOREIGN KEY (start_time) REFERENCES time (start_time),
-    FOREIGN KEY (user_id) REFERENCES users (user_id),
-    FOREIGN KEY (song_id) REFERENCES songs (song_id),
-    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 """)
 
@@ -71,25 +69,49 @@ CREATE TABLE time (
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
+songplay_table_insert = (""" 
+INSERT INTO songplays (
+    start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    values (%s, %s, %s, %s, %s, %s, %s, %s)  
 """)
 
-user_table_insert = ("""
+user_table_insert = (""" 
+INSERT INTO users (
+    user_id, first_name, last_name, gender, level)
+    values (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id)
+DO NOTHING    
 """)
 
-song_table_insert = ("""
+song_table_insert = (""" 
+INSERT INTO songs (song_id, title, artist_id, year, duration) values (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id)
+DO NOTHING
 """)
 
-artist_table_insert = ("""
+artist_table_insert = (""" 
+INSERT INTO artists (artist_id, name, location, latitude, longitude) values (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id)
+DO NOTHING
 """)
 
 
-time_table_insert = ("""
+time_table_insert = (""" 
+INSERT INTO time (
+    start_time, hour, day, week, month, year, weekday)
+    values (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time)
+DO NOTHING
 """)
 
 # FIND SONGS
 
 song_select = ("""
+SELECT s.song_id, s.artist_id 
+FROM songs s
+INNER JOIN artists a
+ON a.artist_id = s.artist_id
+WHERE s.title = %s AND a.name = %s AND s.duration = %s
 """)
 
 # QUERY LISTS
