40e014282682bb0677500ba2247a419628daf9bf
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-05-12 14:39:50 -0700

    Code Formatting

--- sql_queries.py
+++ sql_queries.py
@@ -72,25 +72,29 @@ CREATE TABLE time (
 songplay_table_insert = (""" 
 INSERT INTO songplays (
     start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-    values (%s, %s, %s, %s, %s, %s, %s, %s)  
+values (%s, %s, %s, %s, %s, %s, %s, %s)  
 """)
 
 user_table_insert = (""" 
 INSERT INTO users (
     user_id, first_name, last_name, gender, level)
-    values (%s, %s, %s, %s, %s)
+values (%s, %s, %s, %s, %s)
 ON CONFLICT (user_id)
 DO NOTHING    
 """)
 
 song_table_insert = (""" 
-INSERT INTO songs (song_id, title, artist_id, year, duration) values (%s, %s, %s, %s, %s)
+INSERT INTO songs (
+    song_id, title, artist_id, year, duration) 
+values (%s, %s, %s, %s, %s)
 ON CONFLICT (song_id)
 DO NOTHING
 """)
 
 artist_table_insert = (""" 
-INSERT INTO artists (artist_id, name, location, latitude, longitude) values (%s, %s, %s, %s, %s)
+INSERT INTO artists (
+    artist_id, name, location, latitude, longitude) 
+values (%s, %s, %s, %s, %s)
 ON CONFLICT (artist_id)
 DO NOTHING
 """)
@@ -99,7 +103,7 @@ DO NOTHING
 time_table_insert = (""" 
 INSERT INTO time (
     start_time, hour, day, week, month, year, weekday)
-    values (%s, %s, %s, %s, %s, %s, %s)
+values (%s, %s, %s, %s, %s, %s, %s)
 ON CONFLICT (start_time)
 DO NOTHING
 """)
@@ -116,5 +120,7 @@ WHERE s.title = %s AND a.name = %s AND s.duration = %s
 
 # QUERY LISTS
 
-create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [user_table_create, artist_table_create,
+                        song_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop,
+                      song_table_drop, artist_table_drop, time_table_drop]
