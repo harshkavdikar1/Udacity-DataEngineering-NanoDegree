c793f4acace856c808876856e6810899f77a784e
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-05-19 15:15:01 -0700

    Added doc strings

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,36 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drops each table using the queries in `drop_table_queries` list.
+    Args:
+        cur: Cursor object to the database
+        conn: Connection object to the database
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-def create_tables(cur, conn):
+def create_tables(cur, conn):"""
+    Drops each table using the queries in `create_table_queries` list.
+    Args:
+        cur: Cursor object to the database
+        conn: Connection object to the database
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Reads the config file to get connection configurations.
+
+    Establishes connection to the database.
+
+    Drop and create the staging and target tables.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
